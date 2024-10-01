2ecdca9c6c2f47af0af1047476c140f47bf77c0e
zubairaltaf10 <zubairaltaf10@gmail.com>
2024-10-02 02:53:56 +0500

    streaming in open ai

--- client/src/ui/templates/FlowChartPageTemplate/Index.tsx
+++ client/src/ui/templates/FlowChartPageTemplate/Index.tsx
@@ -47,15 +47,18 @@ const FlowChartPageTemplate: React.FC<{
   }, [visibleQuestions]);
 
   const handleAnswer = (questionId: string, answerId: string) => {
-    setAnswers({ ...answers, [questionId]: answerId });
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [questionId]: answerId,
+    }));
+
     if (answers[questionId]) return;
+
     if (visibleQuestions < questions.length) {
       setVisibleQuestions(visibleQuestions + 1);
     } else {
-      setShowUserInfoForm(true);
-    }
+      setShowUserInfoForm(true);     }
 
-    // Reveal the next question if available
     if (visibleQuestions < questions.length) {
       setVisibleQuestions(visibleQuestions + 1);
     } else {
