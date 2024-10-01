3d55220195685ef9604551c77371a5db69f7b9b4
zubairaltaf10 <zubairaltaf10@gmail.com>
2024-10-01 19:21:22 +0500

    ai logo and some tweaks

--- client/src/ui/atoms/Typography/Subtitle/Index.tsx
+++ client/src/ui/atoms/Typography/Subtitle/Index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Cn } from "../../../../utils/twCn";
 
-const Subtitle: React.FC<{
+const Subtitle = React.forwardRef<HTMLHeadingElement, {
   children: React.ReactNode;
   className?: string | { [key: string]: string };
-}> = ({ children, className }) => {
-  return <h1 className={Cn("text-subtitle", className)}>{children}</h1>;
-};
+}>(({ children, className }, ref) => {
+  return (
+    <h1 ref={ref} className={Cn("text-subtitle", className)}>
+      {children}
+    </h1>
+  );
+});
 
 export default Subtitle;
--- client/src/ui/molecules/ProgramDetails/Index.tsx
+++ client/src/ui/molecules/ProgramDetails/Index.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import { Cn } from "../../../utils/twCn";
 import SelectedItem from "../SelectedItem/Index";
 import { Question } from "../../../types";
+import logo from "./../../../assets/images/kwikAI.png";
+
+const kwikAI = () => {
+  return (
+    <div className="mt-auto flex flex-col gap-0.5">
+      <p className="font-geologica text-[12px]">Powered by</p>
+      <img className="w-20" src={logo} alt="OpenAI" />
+    </div>
+  );
+};
+
 
 const ProgramDetails: React.FC<{
   className?: string | { [key: string]: string };
@@ -9,12 +20,14 @@ const ProgramDetails: React.FC<{
   answers: { [key: string]: string };
 }> = ({ className, questions, answers }) => {
   return (
+    <div className={Cn(className,'p-6 rounded-lg bg-light-cream border border-bright-yellow ')}>
+      <div className="mt-2">
+      <img className="w-20 h-7" src={logo} alt="OpenAI" />
+    </div>
     <div
-      className={Cn(
-        className,
-        "p-10 rounded-lg bg-light-cream border border-bright-yellow grid grid-cols-3 gap-5"
-      )}
+      className="grid grid-cols-3 gap-5 mt-7"
     >
+     
       {questions.map(({ id, text, options }) => {
         const answer = options.find(
           (option) => option.id === answers[id]
@@ -28,6 +41,7 @@ const ProgramDetails: React.FC<{
           />
         );
       })}
+    </div>
     </div>
   );
 };
